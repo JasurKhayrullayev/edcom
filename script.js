73d0756108f7f82d870d440145724819527f6cae
JasurKhayrullayev <jasurkhayrullayev2002@gmail.com>
2025-01-04 17:21:12 +0500

    cc

--- script.js
+++ script.js
@@ -22,10 +22,13 @@ saveToPhoneBtn.addEventListener('click', () => {
 BEGIN:VCARD
 VERSION:3.0
 FN:Abboskhon Makhamatov
-ORG:Edcom
+ORG:Entrepreneurship Development Company
 TITLE:Director
-TEL;TYPE=WORK,VOICE:+998901234567
+TEL;TYPE=WORK,VOICE:+998909467711
+TEL;TYPE=DOM,VOICE:+998712055757
 EMAIL:a.makhamatov@edcom.uz
+URL:https://edcom.uz/
+PHOTO;ENCODING=b;TYPE=JPEG:${imageToBase64('img/img.jpg')}
 END:VCARD
 `;
 
@@ -41,3 +44,23 @@ END:VCARD
     document.body.removeChild(a);
     URL.revokeObjectURL(url);
 });
+
+function imageToBase64(imagePath) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.src = imagePath;
+
+    img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        return canvas.toDataURL('image/jpeg').replace(/^data:image\/jpeg;base64,/, '');
+    };
+
+    img.onerror = () => {
+        console.error('Error loading image.');
+    };
+
+    return '';
+}
